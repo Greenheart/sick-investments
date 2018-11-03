d1a3fa99772f26ab6959cc883fb309f883a52c23
Samuel Plumppu <greenheartmusic@gmail.com>
2018-11-03 23:20:56 +0100

    feature(rapid fire): WIP allow player to skip days quickly.

--- js/game.js
+++ js/game.js
@@ -49,10 +49,97 @@ class Game {
             }
         })
 
-        // Bind button#next-day to the space-key.
-        this.ui.nextDay.addEventListener('click', event => {
+        this.initializeRapidFire()
+    }
+
+
+
+// isStarting = false
+// interval = null
+// shouldStart = true
+
+
+// on pointerdown
+    // if not already running an interval && if not already starting interval
+    // !interval running && shouldStart
+        // queue new interval
+        // isStarting = true
+
+// when starting new interval
+    // interval = new Interval()
+    // isStarting = false
+
+// on pointerup
+    // if interval running
+        // stop interval
+        // shouldStart = true
+    // if isStarting
+        // shouldStart = false
+
+// on pointerout
+    // if interval running
+        // stop interval
+        // shouldStart = true
+    // if isStarting
+        // shouldStart = false
+
+// Potentially, also add the following to allow rapidFire to reset state and work for multiple uses:
+
+// on pointerenter (or similar to mouseenter)
+    // if !interval running && !isStarting
+        // shouldStart = true
+
+
+
+
+
+
+
+
+
+
+
+
+
+    initializeRapidFire () {
+        // The interval that triggers rapid fire. Used to abort rapid fire.
+        let rapidFire = null
+        // Only start rapidFire once at a time.
+        let alreadyStarting = false
+        // The timeout that starts rapid fire. Used when aborting rapid fire before it's even started.
+        let rapidFireTimeout = null
+
+        const startRapidFire = ()  => {
+            // 143 ms between each fire to give ~7 executions per second.
+            console.log('Already Starting: ', alreadyStarting)
+            if (!alreadyStarting && !rapidFire) {
+                rapidFire = window.setInterval(() => this.nextDay(), 143)
+                alreadyStarting = true
+            }
+        }
+
+        const stopRapidFire = () => {
+            console.log('STOP ', rapidFire)
+            // Cancel rapid fire before it's even started.
+            if (rapidFireTimeout) {
+                window.clearTimeout(rapidFireTimeout)
+            }
+
+            if (rapidFire) {
+                window.clearInterval(rapidFire)
+                alreadyStarting = false
+                rapidFire = null
+            }
+        }
+
+        // TODO: try to remove alreadyStarting and replace it with the clearTimeout solution below.
+
+        this.ui.nextDay.addEventListener('pointerdown', event => {
             this.nextDay()
+            rapidFireTimeout = window.setTimeout(startRapidFire, 500)
         })
+        this.ui.nextDay.addEventListener('click', stopRapidFire)
+        this.ui.nextDay.addEventListener('pointerout', stopRapidFire)
     }
 
     getDOMReferences () {
